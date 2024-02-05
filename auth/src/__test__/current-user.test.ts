63e7742bc17cf1e34091406c44f3e7a80800e41a
srnsatya <bestsatyam@gmail.com>
2024-02-04 19:25:36 -0500

    corrected the response code

--- auth/src/__test__/current-user.test.ts
+++ auth/src/__test__/current-user.test.ts
@@ -18,7 +18,7 @@ it('returns current User',async()=>{
 	const currentUserresponse =await request(app)
 					.get('/api/users/currentuser')
 					.send()
-					.expect(401)
+					.expect(200)
 	
 	console.log('current user body without signup',currentUserresponse.body)
 //	expect(currentUserresponse.body.currentUser).toEqual(null)
